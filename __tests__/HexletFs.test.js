16d74f6fc942f72d1692b737ed61cc0e84a7d03c
warpedrhubarb <haveanicedayy420@gmail.com>
2021-05-01 21:47:07 +0300

    add copySync + tests

--- __tests__/HexletFs.test.js
+++ __tests__/HexletFs.test.js
@@ -1,40 +1,40 @@
 import HexletFs from '../src/HexletFs';
 
-// describe('FS', () => {
-//   let files;
-//
-//   beforeEach(() => {
-//     files = new HexletFs();
-//   });
-//
-//   it('#mkdirSync', () => {
-//     files.mkdirSync('/etc');
-//     expect(files.statSync('/etc').isDirectory()).toBeTruthy();
-//
-//     files.mkdirSync('/etc/nginx');
-//     expect(files.statSync('/etc/nginx').isDirectory()).toBeTruthy();
-//   });
-//
-//   it('#mkdirSync2', () => {
-//     files.mkdirSync('/var/');
-//     expect(files.statSync('/var////').isDirectory()).toBeTruthy();
-//     expect(files.statSync('/var').isDirectory()).toBeTruthy();
-//
-//     files.mkdirSync('/var//log//////');
-//     expect(files.statSync('/var/log').isDirectory()).toBeTruthy();
-//     expect(files.statSync('/var///log').isDirectory()).toBeTruthy();
-//   });
-//
-//   it('#touchSync', () => {
-//     files.touchSync('/file.txt');
-//     expect(files.statSync('/file.txt').isFile()).toBeTruthy();
-//
-//     files.mkdirSync('/etc');
-//     files.touchSync('/etc/bashrc');
-//     expect(files.statSync('/etc/bashrc').isFile()).toBeTruthy();
-//   });
-// });
-//
+describe('FS', () => {
+  let files;
+
+  beforeEach(() => {
+    files = new HexletFs();
+  });
+
+  it('#mkdirSync', () => {
+    files.mkdirSync('/etc');
+    expect(files.statSync('/etc').isDirectory()).toBeTruthy();
+
+    files.mkdirSync('/etc/nginx');
+    expect(files.statSync('/etc/nginx').isDirectory()).toBeTruthy();
+  });
+
+  it('#mkdirSync2', () => {
+    files.mkdirSync('/var/');
+    expect(files.statSync('/var////').isDirectory()).toBeTruthy();
+    expect(files.statSync('/var').isDirectory()).toBeTruthy();
+
+    files.mkdirSync('/var//log//////');
+    expect(files.statSync('/var/log').isDirectory()).toBeTruthy();
+    expect(files.statSync('/var///log').isDirectory()).toBeTruthy();
+  });
+
+  it('#touchSync', () => {
+    files.touchSync('/file.txt');
+    expect(files.statSync('/file.txt').isFile()).toBeTruthy();
+
+    files.mkdirSync('/etc');
+    files.touchSync('/etc/bashrc');
+    expect(files.statSync('/etc/bashrc').isFile()).toBeTruthy();
+  });
+});
+
 // describe('FS1', () => {
 //   let files;
 //
@@ -86,68 +86,121 @@ import HexletFs from '../src/HexletFs';
 //   });
 // });
 
-describe('FS2', () => {
+// describe('FS2', () => {
+//   let files;
+//
+//   beforeEach(() => {
+//     files = new HexletFs();
+//     files.mkdirpSync('/etc');
+//     files.mkdirpSync('/opt');
+//     files.touchSync('/opt/file.txt');
+//     files.mkdirpSync('/etc/nginx/conf.d');
+//   });
+//
+//   it('#writeFileSync', () => {
+//     const [data, err] = files.writeFileSync('/etc/unknown/file', 'body');
+//     expect(data).toBe(null);
+//     expect(err.code).toBe('ENOENT');
+//
+//     const [data2, err2] = files.writeFileSync('/etc', 'body');
+//     expect(data2).toBe(null);
+//     expect(err2.code).toBe('EISDIR');
+//
+//     const [data3, err3] = files.writeFileSync('/opt/file.txt/wrong', 'body');
+//     expect(data3).toBe(null);
+//     expect(err3.code).toBe('ENOTDIR');
+//   });
+//
+//   it('#readFileSync', () => {
+//     files.writeFileSync('/etc/nginx/nginx.conf', 'directives');
+//     const [data, err] = files.readFileSync('/etc/nginx/nginx.conf');
+//     expect(data).toBe('directives');
+//     expect(err).toBe(null);
+//
+//     const [data2, err2] = files.readFileSync('/etc/nginx');
+//     expect(data2).toBe(null);
+//     expect(err2.code).toBe('EISDIR');
+//
+//     const [data3, err3] = files.readFileSync('/etc/unknown');
+//     expect(data3).toBe(null);
+//     expect(err3.code).toBe('ENOENT');
+//   });
+//
+//   it('#unlinkSync', () => {
+//     files.writeFileSync('/etc/nginx/nginx.conf', 'directives');
+//     files.unlinkSync('/etc/nginx/nginx.conf');
+//     const [data, err] = files.readdirSync('/etc/nginx');
+//     expect(err).toBe(null);
+//     expect(data).toEqual(['conf.d']);
+//
+//     const [data2, err2] = files.unlinkSync('/etc/nginx');
+//     expect(data2).toBe(null);
+//     expect(err2.code).toBe('EPERM');
+//
+//     const [data3, err3] = files.unlinkSync('/etc/nginx/unexist.file');
+//     expect(data3).toBe(null);
+//     expect(err3.code).toBe('ENOENT');
+//   });
+//
+//   it('#writeFileSync&readFileSync', () => {
+//     const [data, err] = files.writeFileSync('/opt/another-file.txt', 'body');
+//     expect(data.getMeta().getName()).toBe('another-file.txt');
+//     expect(err).toBe(null);
+//     const [data2, err2] = files.readFileSync('/opt/another-file.txt');
+//     expect(data2).toBe('body');
+//     expect(err2).toBe(null);
+//   });
+// });
+
+describe('FS3', () => {
   let files;
 
   beforeEach(() => {
     files = new HexletFs();
-    files.mkdirpSync('/etc');
+    files.mkdirpSync('/etc/nginx');
     files.mkdirpSync('/opt');
     files.touchSync('/opt/file.txt');
     files.mkdirpSync('/etc/nginx/conf.d');
+    files.touchSync('/etc/nginx/nginx.conf');
   });
 
-  it('#writeFileSync', () => {
-    const [data, err] = files.writeFileSync('/etc/unknown/file', 'body');
-    expect(data).toBe(null);
-    expect(err.code).toBe('ENOENT');
+  it('#copySync', () => {
+    expect(() => files.copySync('undefined', '/etc'))
+      .toThrow(/ENOENT/);
 
-    const [data2, err2] = files.writeFileSync('/etc', 'body');
-    expect(data2).toBe(null);
-    expect(err2.code).toBe('EISDIR');
+    expect(() => files.copySync('/opt', '/etc')).toThrow(/EISDIR/);
 
-    const [data3, err3] = files.writeFileSync('/opt/file.txt/wrong', 'body');
-    expect(data3).toBe(null);
-    expect(err3.code).toBe('ENOTDIR');
+    expect(() => files.copySync('/op/file.txt', '/etc/file.txt/inner'))
+      .toThrow(/ENOENT/);
+
+    expect(() => files.copySync('/opt/file.txt', '/etc/undefined/inner'))
+      .toThrow(/ENOENT/);
+
+    files.copySync('/opt/file.txt', '/etc');
+    expect(files.statSync('/etc/file.txt').isFile()).toBeTruthy();
+
+    files.copySync('/opt/file.txt', '/etc/nginx/nginx.conf');
+    expect(files.readFileSync('/etc/nginx/nginx.conf')).toBe('');
   });
 
-  it('#readFileSync', () => {
-    files.writeFileSync('/etc/nginx/nginx.conf', 'directives');
-    const [data, err] = files.readFileSync('/etc/nginx/nginx.conf');
-    expect(data).toBe('directives');
-    expect(err).toBe(null);
+  it('#copySync2', () => {
+    files.writeFileSync('/opt/file.txt', 'body');
+    files.copySync('/opt/file.txt', '/etc/nginx/nginx.conf');
+    expect(files.readFileSync('/etc/nginx/nginx.conf')).toBe('body');
 
-    const [data2, err2] = files.readFileSync('/etc/nginx');
-    expect(data2).toBe(null);
-    expect(err2.code).toBe('EISDIR');
+    files.copySync('/opt/file.txt', '/etc');
+    expect(files.readFileSync('/etc/file.txt')).toBe('body');
 
-    const [data3, err3] = files.readFileSync('/etc/unknown');
-    expect(data3).toBe(null);
-    expect(err3.code).toBe('ENOENT');
+    files.copySync('/opt/file.txt', '/opt/newfile');
+    expect(files.readFileSync('/opt/newfile')).toBe('body');
   });
 
-  it('#unlinkSync', () => {
-    files.writeFileSync('/etc/nginx/nginx.conf', 'directives');
-    files.unlinkSync('/etc/nginx/nginx.conf');
-    const [data, err] = files.readdirSync('/etc/nginx');
-    expect(err).toBe(null);
-    expect(data).toEqual(['conf.d']);
+  it('#copySync3', () => {
+    files.mkdirpSync('/etc/nginx/conf.d');
+    files.touchSync('/etc/nginx/nginx.conf');
+    files.writeFileSync('/opt/file.txt', 'body');
 
-    const [data2, err2] = files.unlinkSync('/etc/nginx');
-    expect(data2).toBe(null);
-    expect(err2.code).toBe('EPERM');
-
-    const [data3, err3] = files.unlinkSync('/etc/nginx/unexist.file');
-    expect(data3).toBe(null);
-    expect(err3.code).toBe('ENOENT');
-  });
-
-  it('#writeFileSync&readFileSync', () => {
-    const [data, err] = files.writeFileSync('/opt/another-file.txt', 'body');
-    expect(data.getMeta().getName()).toBe('another-file.txt');
-    expect(err).toBe(null);
-    const [data2, err2] = files.readFileSync('/opt/another-file.txt');
-    expect(data2).toBe('body');
-    expect(err2).toBe(null);
+    expect(() => files.copySync('/opt/file.txt', '/etc/nginx/nginx.conf/testFile'))
+      .toThrow(/ENOENT/);
   });
 });
--- src/HexletFs.js
+++ src/HexletFs.js
@@ -3,6 +3,7 @@ import errors from 'errno';
 import Tree from './Tree';
 import Dir from './Dir';
 import File from './File';
+import HexletFsError from './HexletFsError.js';
 
 const getPathParts = (filepath) => filepath.split(path.sep).filter((part) => part !== '');
 
@@ -14,47 +15,57 @@ export default class {
   statSync(filepath) {
     const current = this.findNode(filepath);
     if (!current) {
-      return [null, errors.code.ENOENT];
+      throw new HexletFsError(errors.code.ENOENT, filepath);
     }
-    return [current.getMeta().getStats(), null];
+    return current.getMeta().getStats();
+  }
+
+  copySync(src, dest) {
+    const srcBody = this.readFileSync(src);
+    const destNode = this.findNode(dest);
+
+    if (destNode && destNode.getMeta().getStats().isDirectory()) {
+      const { base } = path.parse(src);
+      const fullDest = path.join(dest, base);
+      return this.writeFileSync(fullDest, srcBody);
+    }
+
+    return this.writeFileSync(dest, srcBody);
   }
 
   unlinkSync(filepath) {
     const current = this.findNode(filepath);
     if (!current) {
-      return [null, errors.code.ENOENT];
+      throw new HexletFsError(errors.code.ENOENT, filepath);
     }
     if (current.getMeta().isDirectory()) {
-      return [null, errors.code.EPERM];
+      throw new HexletFsError(errors.code.EPERM, filepath);
     }
-    return [current.getParent().removeChild(current.getKey()), null];
+    return current.getParent().removeChild(current.getKey());
   }
 
   writeFileSync(filepath, content) {
     const { base, dir } = path.parse(filepath);
     const parent = this.findNode(dir);
-    if (!parent) {
-      return [null, errors.code.ENOENT];
-    }
-    if (!parent.getMeta().isDirectory()) {
-      return [null, errors.code.ENOTDIR];
+    if (!parent || !parent.getMeta().isDirectory()) {
+      throw new HexletFsError(errors.code.ENOENT, filepath);
     }
     const current = parent.getChild(base);
     if (current && current.getMeta().isDirectory()) {
-      return [null, errors.code.EISDIR];
+      throw new HexletFsError(errors.code.EISDIR, filepath);
     }
-    return [parent.addChild(base, new File(base, content)), null];
+    return parent.addChild(base, new File(base, content));
   }
 
   readFileSync(filepath) {
     const current = this.findNode(filepath);
     if (!current) {
-      return [null, errors.code.ENOENT];
+      throw new HexletFsError(errors.code.ENOENT, filepath);
     }
     if (!current.getMeta().isFile()) {
-      return [null, errors.code.EISDIR];
+      throw new HexletFsError(errors.code.EISDIR, filepath);
     }
-    return [current.getMeta().getBody(), null];
+    return current.getMeta().getBody();
   }
 
   mkdirSync(filepath) {
@@ -84,24 +95,24 @@ export default class {
   readdirSync(filepath) {
     const current = this.findNode(filepath);
     if (!current) {
-      return [null, errors.code.ENOENT];
+      throw new HexletFsError(errors.code.ENOENT, filepath);
     }
     if (!current.getMeta().isDirectory()) {
-      return [null, errors.code.ENOTDIR];
+      throw new HexletFsError(errors.code.ENOTDIR, filepath);
     }
-    return [current.getChildren().map((child) => child.getKey()), null];
+    return current.getChildren().map((child) => child.getKey());
   }
 
   touchSync(filepath) {
     const { base, dir } = path.parse(filepath);
     const parent = this.findNode(dir);
     if (!parent) {
-      return [null, errors.code.ENOENT];
+      throw new HexletFsError(errors.code.ENOENT, filepath);
     }
     if (!parent.getMeta().isDirectory()) {
-      return [null, errors.code.ENOTDIR];
+      throw new HexletFsError(errors.code.ENOTDIR, filepath);
     }
-    return [parent.addChild(base, new File(base, '')), null];
+    return parent.addChild(base, new File(base, ''));
   }
 
   rmdirSync(filepath) {
